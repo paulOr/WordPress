66765ab61cedc1d2ae968b0ea4f9d1524384d950
Paul Fraser <paulfraser@gmail.com>
2011-02-17 15:13:32 +0000

    wp_shortcodes

--- wp_shortcodes.php
+++ wp_shortcodes.php
@@ -1,16 +1,15 @@
 <?php
-	## CREATE CUSTOM SHORTCODES
-	## [foobar param1="like this" param2="etc"]
-	add_shortcode('foobar', function($atts) {
-		$atts = shortcode_atts(
-			array(
-			'param1' => '',
-			'param2' => '',
-			'param3' => '',
-			'blah....' => ''
-		), $atts);
-     
-		// Print it
-		echo '<div class="whatever"><h1>foobar</h1><p>'.$atts['param1'].'</p></div>';
+	##
+	## CUSTOM SHORTCODES
+	##
+	
+	function foo_func($atts) {
+		extract(shortcode_atts(array(
+			'name' => '',
+			'show' => ''
+		), $atts));
+		return '<p>whatever '.$atts['name'].'</p>';
 	}
+	add_shortcode('foobar', 'foo_func');
+
 ?>
